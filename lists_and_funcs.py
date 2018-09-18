3fed8977cf0dfc829529b4debf7979da5ab04063
Y-CMB <cbarry000@gmail.com>
2018-09-17 22:43:54 -0400

    add fancy ASCII art; ex 8/11

--- lists_and_funcs.py
+++ lists_and_funcs.py
@@ -1,3 +1,15 @@
+##############################################
+##############################################
+##################### | ######################
+################### | | ######################
+################# |   | ######################
+##################### | ######################
+##################### | ######################
+##################### | ######################
+############### -------------- ###############
+##############################################
+##############################################
+
 """
 Create a function named double_index that has two parameters named lst and index.
 
@@ -99,6 +111,19 @@ def append_sum(lst):
 #Uncomment the line below when your function is done
 print(append_sum([1, 1, 2]))
 
-# this is a test
+"""
+Write a function named larger_list that has two parameters named lst1 and lst2.
 
-# this is another test surface
+The function should return the last element of the list that contains more elements. If both lists are the same size, then return the last element of lst1.
+"""
+#Write your function here
+def larger_list(lst1, lst2):
+    if len(lst1) > len(lst2):
+        return lst1[-1]
+    elif len(lst1) < len(lst2):
+        return lst2[-1]
+    else:
+        return lst1[-1]
+
+#Uncomment the line below when your function is done
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
